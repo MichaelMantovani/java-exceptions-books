e0c7a7fb9b38c72f45eb563cefd2515bc9e26800
MichaelMantovani <michmantovani@gmail.com>
2023-11-15 14:14:18 +0100

    create constructor for Book class

--- src/org/java/lessons/Book.java
+++ src/org/java/lessons/Book.java
@@ -1,5 +1,48 @@
 package org.java.lessons;
 
 public class Book {
+	private String title;
+	private int totalPages;
+	private String author;
+	private String editor;
+	
+	public Book(String title, int totalPages, String author, String editor) {
+		setTitle(title);
+		setTotalPages(totalPages);
+		setAuthor(author);
+		setEditor(editor);
+	}
 
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public int getTotalPages() {
+		return totalPages;
+	}
+
+	public void setTotalPages(int totalPages) {
+		this.totalPages = totalPages;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getEditor() {
+		return editor;
+	}
+
+	public void setEditor(String editor) {
+		this.editor = editor;
+	}
+	
 }
